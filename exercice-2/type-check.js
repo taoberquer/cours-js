d643be79065a8827dac4fc3afe010fbd51e1e313
Taopol BERQUER <tberquer@rampar.com>
2020-10-23 17:58:43 +0200

    Correction desn oms de fonctions.

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,8 +1,8 @@
-function type_check_V1(value, type) {
+function type_check_v1(value, type) {
   return typeof value === type;
 }
 
-function type_check_V2(value, checkers) {
+function type_check_v2(value, checkers) {
   if (! 'type' in checkers || type_check_V1(value, checkers.type)) return false;
   if (! 'value' in checkers || value != checkers.value) return false;
   if (! 'enum' in checkers || ! checkers.enum.includes(value)) return false;
