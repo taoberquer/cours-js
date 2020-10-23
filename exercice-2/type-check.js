37ae73a1ca847c1241d8c18ba07690098cbd6e55
Taopol BERQUER <tberquer@rampar.com>
2020-10-23 17:57:16 +0200

    Ajout de type_check V1 et V2

---
+++ exercice-2/type-check.js
@@ -0,0 +1,11 @@
+function type_check_V1(value, type) {
+  return typeof value === type;
+}
+
+function type_check_V2(value, checkers) {
+  if (! 'type' in checkers || type_check_V1(value, checkers.type)) return false;
+  if (! 'value' in checkers || value != checkers.value) return false;
+  if (! 'enum' in checkers || ! checkers.enum.includes(value)) return false;
+
+  return true;
+}
