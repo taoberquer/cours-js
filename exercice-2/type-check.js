0119f89a82f73f9547460905f648238ffa4c9b0c
Taopol BERQUER <tberquer@rampar.com>
2020-10-23 18:27:47 +0200

    Correction de type_check_v1

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,6 +1,9 @@
 function type_check_v1(value, type) {
-  if (type == 'array' && value.isArray()) return false;
-  if (type == 'null' && value === null) return false;
+  if (type == 'array' && ! Array.isArray(value)) return false;
+  if (type == 'null' && value !== null) return false;
+  if (type == 'function' && {}.toString.call(value) !== '[object Function]') return false;
+  if (type == 'arrayNotObject' && (! Array.isArray(value) || typeof value === 'object')) return false;
+  if (type == 'nullNotObject' && (value !== null || typeof value === 'object')) return false;
 
   return typeof value === type;
 }
